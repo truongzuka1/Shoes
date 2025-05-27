2563422d4c7a42e58059ccade427baad8a8482bb
QuangHuy <dontcomeback0372@gmail.com>
2025-05-27 10:18:32 +0700

    comment all

--- Client/Models/ErrorViewModel.cs
+++ Client/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-namespace Client.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+//namespace Client.Models
+//{
+//    public class ErrorViewModel
+//    {
+//        public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
+//        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+//    }
+//}
